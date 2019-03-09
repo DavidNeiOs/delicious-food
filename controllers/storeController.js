815312c30726b7e6cf575c93b05b9b3ee67a875b
DavidNeiOs <davidneios25@gmail.com>
2019-03-08 23:29:38 -0500

    stores mixin and query

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
 exports.homePage = (req, res) => {
   res.render("index");
 };
@@ -6,6 +9,17 @@ exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
 };
 
-exports.createStore = (req, res) => {
-  console.log(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Succesfully created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
 };
