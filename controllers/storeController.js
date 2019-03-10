afcb8bc28b764d977cdbaf5e86582bad4c1646b3
DavidNeiOs <davidneios25@gmail.com>
2019-03-09 22:50:09 -0500

    single store page done

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -82,3 +82,9 @@ exports.updateStore = async (req, res) => {
   // redirect them and tell them that it worked
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render("store", { store, title: store.name });
+};
--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,7 @@ router.post(
   catchErrors(storeController.resize),
   catchErrors(storeController.updateStore)
 );
+
 router.get("/stores/:id/edit", catchErrors(storeController.editStore));
+router.get("/store/:slug", catchErrors(storeController.getStoreBySlug));
 module.exports = router;
